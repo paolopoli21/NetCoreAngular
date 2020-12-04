936c8bd9713bde7933617f93abd048d971677ed6
Paulpoli1 <pauluni10@hotmail.com>
2020-12-04 20:01:52 +0100

    67.Adding Cors Supports to the Api

--- Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -8,6 +8,8 @@ namespace Core.Specifications
     {
         public ProductWithTypesAndBrandsSpecification(ProductSpecParams productParams)
         :base(x => 
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+            (productParams.Search)) && 
             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId
             && (!productParams.TypeId.HasValue || x.ProductBrandId == productParams.TypeId)
             )
