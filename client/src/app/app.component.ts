c51875c2594577eaf026731b11419edc7dad7a5c
Paulpoli1 <pauluni10@hotmail.com>
2020-12-07 11:00:33 +0100

    89.Consuming servics the component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from 'src/app/shared/models/pagination';
-import { IProduct } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +7,10 @@ import { IProduct } from 'src/app/shared/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
 
-  constructor( private http: HttpClient){
-    this.products = [];
+  constructor( ){
   }
 
   ngOnInit(): void {
-    this.http.get<IPagination>('https://localhost:5001/api/products?pageSize=50')
-    .subscribe((response : IPagination) =>{
-      this.products = response.data;
-      console.log(response);
-    },
-    error =>{
-      console.log(error);
-    }
-    );
   }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,12 +6,11 @@ import { IPagination } from '../shared/models/pagination';
   providedIn: 'root'
 })
 export class ShopService {
-  baseUrl = 'https://localhost: 5001/api/';
+  baseUrl = 'https://localhost:5001/api/';
 
   constructor(private http: HttpClient) { }
 
   getProducts(){
     return this.http.get<IPagination>(this.baseUrl + 'products');
-
   }
 }
