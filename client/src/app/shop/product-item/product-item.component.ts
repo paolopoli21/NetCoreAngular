b6767e8e055f1e1284110c3c2d9b128ac2692aac
Paulpoli1 <pauluni10@hotmail.com>
2020-12-07 13:27:04 +0100

    92.Passing dow data to child components

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-product-item',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
-
-  constructor() { }
+  @Input()
+  product!: IProduct;
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
