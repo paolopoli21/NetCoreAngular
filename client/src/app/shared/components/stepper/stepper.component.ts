5f81c28ee882bce4a93a73353ad182322cc44fd8
Paulpoli1 <pauluni10@hotmail.com>
2021-01-21 20:30:38 +0100

    231.Setting up the stepper component

--- client/src/app/shared/components/stepper/stepper.component.ts
+++ client/src/app/shared/components/stepper/stepper.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkStepper } from '@angular/cdk/stepper';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.css']
+  styleUrls: ['./stepper.component.css'],
+  providers: [{provide: CdkStepper, useExisting: StepperComponent}]
 })
-export class StepperComponent implements OnInit {
-
-  constructor() { }
+export class StepperComponent extends CdkStepper implements OnInit {
+  @Input() linearModeSelected: boolean = false;
 
   ngOnInit(): void {
+    this.linear = this.linearModeSelected;
+  }
+
+  onClick(index: number){
+    this.selectedIndex = index;
+    console.log(this.selectedIndex);
+
   }
 
 }
